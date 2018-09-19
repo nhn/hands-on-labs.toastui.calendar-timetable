25e178a9d9ceb2deff1c2705ed6d179521de8d48
Dongsik Yoo <dongsik.yoo@nhnent.com>
2018-09-19 23:30:41 +0900

    chore: 불필요한 sh 파일 삭제

--- rich.sh
+++
@@ -1,2 +0,0 @@
-hook_url=https://hook.dooray.com/services/TODO
-curl -H "Content-Type: application/json" -X POST -d '{"botName": "MyBot", "botIconImage": "https://translate.nhnent.com/icon/botimage.jpg", "text":"TODO_TEXT","attachments":[{"title":"", "titleLink": "", "text":"", "color": "darkgreen"}]}' $hook_url
