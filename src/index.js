c2fd95062aaf85873605370857d374414fad1753
Jenny Cho <jenny.cho@nhn.com>
2019-11-01 20:59:28 +0900

    chore: modify text

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* 코드를 작성해 보세요. */
 
 /* ---------------------------------------------- */
-/* 이동 및 보기 버튼 이벤트 핸들러 */
+/* 이동 및 뷰 타입 변경 버튼 이벤트 핸들러 */
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 const dayViewBtn = document.getElementById('dayViewBtn');
